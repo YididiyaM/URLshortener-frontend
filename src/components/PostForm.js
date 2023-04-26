bd88cab9e59febee1a8822fe30c75e0ae58faa6f
Yididiya Mulatu <yididiyamulatu@gmail.com>
2023-04-26 20:57:28 +0200

    added lodash so every keystroke doesnt trigger a rerender

--- src/components/PostForm.js
+++ src/components/PostForm.js
@@ -1,31 +1,33 @@
 import React, { useState } from "react";
 import axios from "axios";
-
+import { debounce } from "lodash";
 const PostForm = () => {
   const url = "http://localhost:8080/shorturl";
   const [data, setData] = useState("data baby");
 
   const submit = (e) => {
-    axios
-      .post("http://localhost:8080/data", {})
-      .then((response) => {
-        console.log(response.data);
-      })
-      .catch((error) => {
-        console.error(error);
-      });
+    e.target.reset();
+    e.preventDefault();
+    axios.get(url).then((response) => {
+      console.log(response);
+    });
   };
 
-  const handle = (e) => {
+  const handle = debounce((e, url) => {
     const newData = e.target.value;
     setData(newData);
     console.log(newData);
-  };
+  }, 500);
 
   return (
     <div>
       <form onSubmit={(e) => submit(e)}>
-        <input onChange={(e) => handle(e)} id="whatever " value={data} />
+        <input
+          onChange={(e) => handle(e)}
+          id="whatever "
+          placeholder="insert your url here"
+          type="text"
+        />
         <button>Submit</button>
       </form>
     </div>
---
+++ src/components/ShortenedURL.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ShortenedURL = () => {
+  return <div>ShortenedURL</div>;
+};
+
+export default ShortenedURL;
